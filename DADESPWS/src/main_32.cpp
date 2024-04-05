762913518fad623191a2b9fcfed4cffa3959e02a
Luis Miguel Soria Morillo <lsoriamo@gmail.com>
2024-04-05 12:19:42 +0200

    Create main_32.cpp

---
+++ DADESPWS/src/main_32.cpp
@@ -0,0 +1,320 @@
+#include <HTTPClient.h>
+#include "ArduinoJson.h"
+#include <WiFiUdp.h>
+
+// Replace 0 by ID of this current device
+const int DEVICE_ID = 124;
+
+int test_delay = 1000; // so we don't spam the API
+boolean describe_tests = true;
+
+// Replace 0.0.0.0 by your server local IP (ipconfig [windows] or ifconfig [Linux o MacOS] gets IP assigned to your PC)
+String serverName = "http://192.168.1.178/";
+HTTPClient http;
+
+// Replace WifiName and WifiPassword by your WiFi credentials
+#define STASSID "Your_Wifi_SSID"    //"Your_Wifi_SSID"
+#define STAPSK "Your_Wifi_PASSWORD" //"Your_Wifi_PASSWORD"
+
+// Setup
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(STASSID);
+
+  /* Explicitly set the ESP32 to be a WiFi-client, otherwise, it by default,
+     would try to act as both a client and an access-point and could cause
+     network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(STASSID, STAPSK);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Setup!");
+}
+
+String response;
+
+String serializeSensorValueBody(int idSensor, long timestamp, float value)
+{
+  // StaticJsonObject allocates memory on the stack, it can be
+  // replaced by DynamicJsonDocument which allocates in the heap.
+  //
+  DynamicJsonDocument doc(2048);
+
+  // Add values in the document
+  //
+  doc["idSensor"] = idSensor;
+  doc["timestamp"] = timestamp;
+  doc["value"] = value;
+  doc["removed"] = false;
+
+  // Generate the minified JSON and send it to the Serial port.
+  //
+  String output;
+  serializeJson(doc, output);
+  Serial.println(output);
+
+  return output;
+}
+
+String serializeActuatorStatusBody(float status, bool statusBinary, int idActuator, long timestamp)
+{
+  DynamicJsonDocument doc(2048);
+
+  doc["status"] = status;
+  doc["statusBinary"] = statusBinary;
+  doc["idActuator"] = idActuator;
+  doc["timestamp"] = timestamp;
+  doc["removed"] = false;
+
+  String output;
+  serializeJson(doc, output);
+  return output;
+}
+
+String serializeDeviceBody(String deviceSerialId, String name, String mqttChannel, int idGroup)
+{
+  DynamicJsonDocument doc(2048);
+
+  doc["deviceSerialId"] = deviceSerialId;
+  doc["name"] = name;
+  doc["mqttChannel"] = mqttChannel;
+  doc["idGroup"] = idGroup;
+
+  String output;
+  serializeJson(doc, output);
+  return output;
+}
+
+void deserializeActuatorStatusBody(String responseJson)
+{
+  if (responseJson != "")
+  {
+    DynamicJsonDocument doc(2048);
+
+    // Deserialize the JSON document
+    DeserializationError error = deserializeJson(doc, responseJson);
+
+    // Test if parsing succeeds.
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    // Fetch values.
+    int idActuatorState = doc["idActuatorState"];
+    float status = doc["status"];
+    bool statusBinary = doc["statusBinary"];
+    int idActuator = doc["idActuator"];
+    long timestamp = doc["timestamp"];
+
+    Serial.println(("Actuator status deserialized: [idActuatorState: " + String(idActuatorState) + ", status: " + String(status) + ", statusBinary: " + String(statusBinary) + ", idActuator" + String(idActuator) + ", timestamp: " + String(timestamp) + "]").c_str());
+  }
+}
+
+void deserializeDeviceBody(int httpResponseCode)
+{
+
+  if (httpResponseCode > 0)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String responseJson = http.getString();
+    DynamicJsonDocument doc(2048);
+
+    DeserializationError error = deserializeJson(doc, responseJson);
+
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    int idDevice = doc["idDevice"];
+    String deviceSerialId = doc["deviceSerialId"];
+    String name = doc["name"];
+    String mqttChannel = doc["mqttChannel"];
+    int idGroup = doc["idGroup"];
+
+    Serial.println(("Device deserialized: [idDevice: " + String(idDevice) + ", name: " + name + ", deviceSerialId: " + deviceSerialId + ", mqttChannel" + mqttChannel + ", idGroup: " + idGroup + "]").c_str());
+  }
+  else
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+}
+
+void deserializeSensorsFromDevice(int httpResponseCode)
+{
+
+  if (httpResponseCode > 0)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String responseJson = http.getString();
+    // allocate the memory for the document
+    DynamicJsonDocument doc(ESP.getMaxAllocHeap());
+
+    // parse a JSON array
+    DeserializationError error = deserializeJson(doc, responseJson);
+
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    // extract the values
+    JsonArray array = doc.as<JsonArray>();
+    for (JsonObject sensor : array)
+    {
+      int idSensor = sensor["idSensor"];
+      String name = sensor["name"];
+      String sensorType = sensor["sensorType"];
+      int idDevice = sensor["idDevice"];
+
+      Serial.println(("Sensor deserialized: [idSensor: " + String(idSensor) + ", name: " + name + ", sensorType: " + sensorType + ", idDevice: " + String(idDevice) + "]").c_str());
+    }
+  }
+  else
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+}
+
+void deserializeActuatorsFromDevice(int httpResponseCode)
+{
+
+  if (httpResponseCode > 0)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String responseJson = http.getString();
+    // allocate the memory for the document
+    DynamicJsonDocument doc(ESP.getMaxAllocHeap());
+
+    // parse a JSON array
+    DeserializationError error = deserializeJson(doc, responseJson);
+
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+
+    // extract the values
+    JsonArray array = doc.as<JsonArray>();
+    for (JsonObject sensor : array)
+    {
+      int idActuator = sensor["idActuator"];
+      String name = sensor["name"];
+      String actuatorType = sensor["actuatorType"];
+      int idDevice = sensor["idDevice"];
+
+      Serial.println(("Actuator deserialized: [idActuator: " + String(idActuator) + ", name: " + name + ", actuatorType: " + actuatorType + ", idDevice: " + String(idDevice) + "]").c_str());
+    }
+  }
+  else
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+}
+
+void test_response(int httpResponseCode)
+{
+  delay(test_delay);
+  if (httpResponseCode > 0)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println(payload);
+  }
+  else
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+}
+
+void describe(char *description)
+{
+  if (describe_tests)
+    Serial.println(description);
+}
+
+void GET_tests()
+{
+  describe("Test GET full device info");
+  String serverPath = serverName + "api/devices/" + String(DEVICE_ID);
+  http.begin(serverPath.c_str());
+  // test_response(http.GET());
+  deserializeDeviceBody(http.GET());
+
+  describe("Test GET sensors from deviceID");
+  serverPath = serverName + "api/devices/" + String(DEVICE_ID) + "/sensors";
+  http.begin(serverPath.c_str());
+  deserializeSensorsFromDevice(http.GET());
+
+  describe("Test GET actuators from deviceID");
+  serverPath = serverName + "api/devices/" + String(DEVICE_ID) + "/actuators";
+  http.begin(serverPath.c_str());
+  deserializeActuatorsFromDevice(http.GET());
+
+  describe("Test GET sensors from deviceID and Type");
+  serverPath = serverName + "api/devices/" + String(DEVICE_ID) + "/sensors/Temperature";
+  http.begin(serverPath.c_str());
+  deserializeSensorsFromDevice(http.GET());
+
+  describe("Test GET actuators from deviceID");
+  serverPath = serverName + "api/devices/" + String(DEVICE_ID) + "/actuators/Relay";
+  http.begin(serverPath.c_str());
+  deserializeActuatorsFromDevice(http.GET());
+}
+
+void POST_tests()
+{
+  String actuator_states_body = serializeActuatorStatusBody(random(2000, 4000) / 100, true, 1, millis());
+  describe("Test POST with actuator state");
+  String serverPath = serverName + "api/actuator_states";
+  http.begin(serverPath.c_str());
+  test_response(http.POST(actuator_states_body));
+
+  String sensor_value_body = serializeSensorValueBody(18, millis(), random(2000, 4000) / 100);
+  describe("Test POST with sensor value");
+  serverPath = serverName + "api/sensor_values";
+  http.begin(serverPath.c_str());
+  test_response(http.POST(sensor_value_body));
+
+  // String device_body = serializeDeviceBody(String(DEVICE_ID), ("Name_" + String(DEVICE_ID)).c_str(), ("mqtt_" + String(DEVICE_ID)).c_str(), 12);
+  // describe("Test POST with path and body and response");
+  // serverPath = serverName + "api/device";
+  // http.begin(serverPath.c_str());
+  // test_response(http.POST(actuator_states_body));
+}
+
+// Run the tests!
+void loop()
+{
+  GET_tests();
+  POST_tests();
+}
